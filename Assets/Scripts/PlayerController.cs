989caaca888166a8b69c3ffcc53cc2bb5174188d
Yanis Verbeke <yanis88357@gmail.com>
2021-07-05 16:40:31 +0200

    YV - Add Input Manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -25,41 +27,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position += new Vector3(Input.GetAxis("Horizontal") * _speedForce * Time.deltaTime, 0, 0);
-        _rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * _speedForce, _rigidbody.velocity.y, 0);
+        /*_rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * _speedForce, _rigidbody.velocity.y, 0);
 
-        if (Input.GetButton("Jump") && _onGrounded)
-        {
-            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _onGrounded = false;
-        }
-
+        */
         if (_rigidbody.velocity.y < 0)
         {
             _rigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (_rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        _rigidbody.velocity = new Vector3(value.Get<Vector2>().x * _speedForce, _rigidbody.velocity.y, 0);
+    }
+
+    private void OnJump()
+    {
+        Debug.Log("Jump");
+        if (_onGrounded)
         {
-            _rigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            Debug.Log(transform.position.y);
+            _onGrounded = false;
+            Debug.Log("en l air ");
+
         }
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
         {
             foreach (ContactPoint hitPos in collision.contacts)
             {
-                Debug.Log(hitPos.normal.ToString());
                 if (hitPos.normal.x != 0) // check if the wall collided on the sides
                 {
                     _onGrounded = false; // boolean to prevent player from being able to jump
                 }
                 else if (hitPos.normal.y > 0) // check if its collided on top 
                 {
-                    _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, 0);
+                    //_rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, 0);
                     _onGrounded = true;
-                    print("grounded");
                 }
                 else _onGrounded = false;
             }
