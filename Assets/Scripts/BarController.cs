d51fa52ff13b7ec7f96c4609003b7962678a8116
Tim Torres <timothe.torres.0505@gmail.com>
2021-07-06 14:24:42 +0200

    Bar avec timer + changement de position

--- Assets/Scripts/BarController.cs
+++ Assets/Scripts/BarController.cs
@@ -6,19 +6,39 @@ public class BarController : MonoBehaviour
 {
     public GameObject barPrefab;
     private GameObject currentBar;
+    [SerializeField] int MinTimeSpawn;
+    [SerializeField] int MaxTimeSpawn;
+    private float TimeSpawnBar;
+    private int BarPosition;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        currentBar = Instantiate(barPrefab, new Vector3(0, 10, 0), new Quaternion());
+        TimeSpawnBar = Time.time;
+        //currentBar = Instantiate(barPrefab, new Vector3(0, 10, 0), new Quaternion());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentBar.transform.position.y < -1)
+
+        if(Time.time > TimeSpawnBar)
         {
-            currentBar = Instantiate(barPrefab, new Vector3(0, 10, 0), new Quaternion());
-        }   
+            BarPosition = Random.Range(0, 2);
+            Debug.Log(BarPosition);
+            if (BarPosition == 0)
+            {
+                currentBar = Instantiate(barPrefab, new Vector3(0, 10, 0), new Quaternion());
+            }
+            else
+            {
+                currentBar = Instantiate(barPrefab, new Vector3(20, 10, 0), new Quaternion());
+            }
+            TimeSpawnBar = Random.Range(MinTimeSpawn, MaxTimeSpawn) + Time.time;
+        }
+            
+    
     }
 }
