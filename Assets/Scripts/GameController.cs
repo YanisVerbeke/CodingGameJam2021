679396d0c1bd473c4a38259bfc41189cc5dae2f2
MathieuMac <mathieusoufflard@gmail.com>
2021-07-08 16:23:51 +0200

    MS : ajout dans le script GameControleur charge 2 prefabe aleatoir dans une liste de prefabe

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
+        LoadMap();
         for (int i = 0; i < PlayerState.playerList.Count; i++)
         {
             int x_position = i == 0 ? -12 : 12 ;
@@ -20,17 +22,19 @@ public class GameController : MonoBehaviour
 
     }
 
+    private void LoadMap()
+    {
+        System.Random random = new System.Random();
+
+        int num = random.Next(0, map.Count);
+        Instantiate(map[num], new Vector3(-12, 0, 0), new Quaternion());
+        int num2 = random.Next(0, 1);
+        Instantiate(map[num2], new Vector3(12, 0, 0), new Quaternion());
+    }
+
     // Update is called once per frame
     void Update()
     {
         
     }
-
-    void LoadLevel()
-    {
-        System.Random random = new System.Random();
-        int num = random.Next(0, 1);
-        SceneManager.LoadScene(map[num].ToString());        
-    }
-
 }
